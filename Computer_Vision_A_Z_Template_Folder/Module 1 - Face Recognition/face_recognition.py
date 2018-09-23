860d7d4286e662c96d0ea3b744758a6b12cff583
Gregor Läufer <gregor.laeufer@gmail.com>
2018-09-23 17:04:07 +0200

    Fresh start

--- Computer_Vision_A_Z_Template_Folder/Module 1 - Face Recognition/face_recognition.py
+++ Computer_Vision_A_Z_Template_Folder/Module 1 - Face Recognition/face_recognition.py
@@ -12,3 +12,9 @@ Created on Sun Sep 16 18:07:50 2018
 import cv2 
 
 #loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#Defining a function that does the detections
+def detect(gray, frame):
+    faces = face_cascade.
